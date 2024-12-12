2fc860d7d8ff0782c618c6b7986973439169bc2b
WADEPAR6 <kparedes9203@uta.edu.ec>
2024-12-12 14:14:48 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"appparedes-767a3","appId":"1:505537997477:web:ceba294c7838f08065fb4c","storageBucket":"appparedes-767a3.firebasestorage.app","apiKey":"AIzaSyBui48JqaxD7h-OyMuJl4jt2bTPCfsbJ6o","authDomain":"appparedes-767a3.firebaseapp.com","messagingSenderId":"505537997477"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

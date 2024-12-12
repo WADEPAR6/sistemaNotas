ceb4bf4d01de6cadb08f3a11ee4f747efc63f726
KevinGanan <Kevinganan2901@gmail.com>
2024-12-12 16:13:53 -0500

    Calificaciones y Matriculas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,19 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'calificaciones',
+    loadChildren: () => import('./pages/calificaciones/calificaciones.module').then(m => m.CalificacionesPageModule)
+  },
 ];
+
 
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,AngularFirestoreModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"appparedes-767a3","appId":"1:505537997477:web:ceba294c7838f08065fb4c","storageBucket":"appparedes-767a3.firebasestorage.app","apiKey":"AIzaSyBui48JqaxD7h-OyMuJl4jt2bTPCfsbJ6o","authDomain":"appparedes-767a3.firebaseapp.com","messagingSenderId":"505537997477"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
